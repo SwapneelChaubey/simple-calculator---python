2d3b7f02515491778ce24249d59671d01aeae51f
Swapneel Chaubey <swapneelchaubey108@gmail.com>
2024-10-08 10:40:43 +0530

    Add files via upload

---
+++ day7&8.py
@@ -0,0 +1,22 @@
+# project 1 
+# Calculator
+a= int(input("Enter the firts number : "))
+b= int(input("Enter the second number : "))
+
+add = a+b
+print("\nThe addition of two number is :",add)
+
+sub = a-b
+print("The subtraction of two number is :",sub)
+
+mul = a*b
+print("The multiplication of two number is :",mul)
+
+div = a/b
+print("The division of two number is :",div)
+
+modulo = a%b
+print("The modulo of two number is :",modulo)
+
+power = a**b
+print("The power of two number is :",power)
